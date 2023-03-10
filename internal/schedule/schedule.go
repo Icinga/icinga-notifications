aca9a26a3bb63f76b8ffe876e30900582af06250
Julian Brost <julian.brost@icinga.com>
2023-03-10 11:38:23 +0100

    Add function to expand a schedule to contacts at a time

--- internal/schedule/schedule.go
+++ internal/schedule/schedule.go
@@ -3,6 +3,7 @@ package schedule
 import (
 	"github.com/icinga/noma/internal/contact"
 	"github.com/icinga/noma/internal/timeperiod"
+	"time"
 )
 
 type Schedule struct {
@@ -14,4 +15,23 @@ type Member struct {
 	TimePeriod   *timeperiod.TimePeriod
 	Contact      *contact.Contact
 	ContactGroup *contact.Group
+}
+
+// GetContactsAt returns the contacts that are active in the schedule at the given time.
+func (s *Schedule) GetContactsAt(t time.Time) []*contact.Contact {
+	var contacts []*contact.Contact
+
+	for _, m := range s.Members {
+		if m.TimePeriod.Contains(t) {
+			if m.Contact != nil {
+				contacts = append(contacts, m.Contact)
+			}
+
+			if m.ContactGroup != nil {
+				contacts = append(contacts, m.ContactGroup.Members...)
+			}
+		}
+	}
+
+	return contacts
 }
