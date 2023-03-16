096d098527aae37de7d30d7bba9ec87395e37ac2
Julian Brost <julian.brost@icinga.com>
2023-03-16 13:40:13 +0100

    Implement a fallback to recipients for escalation names

--- internal/rule/escalation.go
+++ internal/rule/escalation.go
@@ -3,6 +3,7 @@ package rule
 import (
 	"github.com/icinga/noma/internal/contact"
 	"github.com/icinga/noma/internal/schedule"
+	"strings"
 )
 
 type Escalation struct {
@@ -14,4 +15,28 @@ type Escalation struct {
 	Contacts      []*contact.Contact
 	ContactGroups []*contact.Group
 	Schedules     []*schedule.Schedule
+}
+
+func (e *Escalation) DisplayName() string {
+	if e.Name != "" {
+		return e.Name
+	}
+
+	var recipients []string
+
+	for _, c := range e.Contacts {
+		recipients = append(recipients, "[C] "+c.FullName)
+	}
+	for _, g := range e.ContactGroups {
+		recipients = append(recipients, "[G] "+g.Name)
+	}
+	for _, s := range e.Schedules {
+		recipients = append(recipients, "[S] "+s.Name)
+	}
+
+	if len(recipients) == 0 {
+		return "(no recipients)"
+	}
+
+	return strings.Join(recipients, ", ")
 }
