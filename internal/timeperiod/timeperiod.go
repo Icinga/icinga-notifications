e2fc99883cacdff2f46df9aca7a2537714b801c0
Yonas Habteab <yonas.habteab@icinga.com>
2023-03-20 12:58:56 +0100

    Implement `Entry#,Timeperiod#NextTransition()` methods with RRule evaluation

--- internal/timeperiod/timeperiod.go
+++ internal/timeperiod/timeperiod.go
@@ -1,6 +1,10 @@
 package timeperiod
 
-import "time"
+import (
+	"github.com/teambition/rrule-go"
+	"log"
+	"time"
+)
 
 type TimePeriod struct {
 	Name    string
@@ -24,23 +28,61 @@ func (p *TimePeriod) Contains(t time.Time) bool {
 // i.e. the earliest time a change happens, is at the returned time. Note that for simplicity of the implementation,
 // this specification does not require that a transition happens at the returned time.
 func (p *TimePeriod) NextTransition(base time.Time) time.Time {
-	// TODO: implement this in a more useful way once RRULE evaluation is implemented
-	return base.Add(time.Second).Truncate(time.Second)
+	transition := base.Add(24 * time.Hour)
+	for _, e := range p.Entries {
+		next := e.NextTransition(base)
+		if next.Before(transition) && !next.IsZero() {
+			// When the next transition of the previous entry is after the
+			// current one's next transition, prefer the current one.
+			transition = next
+		}
+	}
+
+	return transition
 }
 
 type Entry struct {
 	Start, End time.Time
 
-	// temporarily used for prototyping purposes
-	RepeatEvery time.Duration
-
 	// for future use
-	TimeZone       string // or *time.Location
+	TimeZone string // or *time.Location
+
 	RecurrenceRule string // RFC5545 RRULE
+	rrule          *rrule.RRule
+}
+
+// Init initializes the rrule instance from the configured rrule string
+func (e *Entry) Init() error {
+	if e.rrule != nil || e.RecurrenceRule == "" {
+		return nil
+	}
+
+	option, err := rrule.StrToROptionInLocation(e.RecurrenceRule, e.Start.Location())
+	if err != nil {
+		return err
+	}
+
+	if option.Dtstart.IsZero() {
+		option.Dtstart = e.Start
+	}
+
+	rule, err := rrule.NewRRule(*option)
+	if err != nil {
+		return err
+	}
+
+	e.rrule = rule
+
+	return nil
 }
 
 // Contains returns whether a point in time t is covered by this entry.
 func (e *Entry) Contains(t time.Time) bool {
+	err := e.Init()
+	if err != nil {
+		log.Printf("Can't initialize entry: %s", err)
+	}
+
 	if t.Before(e.Start) {
 		return false
 	}
@@ -49,11 +91,44 @@ func (e *Entry) Contains(t time.Time) bool {
 		return true
 	}
 
-	// TODO: replace with RRULE evaluation
-	// trivial repetition pattern for testing
-	if e.RepeatEvery > 0 && t.Sub(e.Start)%e.RepeatEvery < e.End.Sub(e.Start) {
-		return true
+	if e.rrule == nil {
+		return false
 	}
 
-	return false
+	lastStart := e.rrule.Before(t, true)
+	lastEnd := lastStart.Add(e.End.Sub(e.Start))
+	// Whether the date time is between the last recurrence start and the last recurrence end
+	return (t.Equal(lastStart) || t.After(lastStart)) && t.Before(lastEnd)
+}
+
+// NextTransition returns the next recurrence start or end of this entry relative to the given time inclusively.
+// This function returns also time.Time's zero value if there is no transition that starts/ends at/after the
+// specified time.
+func (e *Entry) NextTransition(t time.Time) time.Time {
+	err := e.Init()
+	if err != nil {
+		log.Printf("Can't initialize entry: %s", err)
+	}
+
+	if t.Before(e.Start) {
+		// The passed time is before the configured event start time
+		return e.Start
+	}
+
+	if t.Before(e.End) {
+		return e.End
+	}
+
+	if e.rrule == nil {
+		return time.Time{}
+	}
+
+	lastStart := e.rrule.Before(t, true)
+	lastEnd := lastStart.Add(e.End.Sub(e.Start))
+	if (t.Equal(lastStart) || t.After(lastStart)) && t.Before(lastEnd) {
+		// Base time is after the last transition begin but before the last transition end
+		return lastEnd
+	}
+
+	return e.rrule.After(t, false)
 }
