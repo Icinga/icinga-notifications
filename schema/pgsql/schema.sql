fd137e014e43aebd0881912c5151a96407ee3965
Yonas Habteab <yonas.habteab@icinga.com>
2023-04-12 17:25:48 +0200

    Schema: Add `PK` constraint explicitly

--- schema/pgsql/schema.sql
+++ schema/pgsql/schema.sql
@@ -1,48 +1,59 @@
 CREATE TABLE contact (
-    id bigserial PRIMARY KEY,
+    id bigserial,
     full_name text NOT NULL,
-    username text -- reference to web user
+    username text, -- reference to web user
+
+    CONSTRAINT pk_contact PRIMARY KEY (id)
 );
 
 CREATE TABLE contact_address (
-    id bigserial PRIMARY KEY,
+    id bigserial,
     contact_id bigint NOT NULL REFERENCES contact(id),
     type text NOT NULL, -- 'phone', 'email', ...
     address text NOT NULL, -- phone number, email address, ...
 
+    CONSTRAINT pk_contact_address PRIMARY KEY (id),
     UNIQUE (contact_id, type) -- constraint may be relaxed in the future to support multiple addresses per type
 );
 
 CREATE TABLE contactgroup (
-    id bigserial PRIMARY KEY,
-    name text NOT NULL
+    id bigserial,
+    name text NOT NULL,
+
+    CONSTRAINT pk_contactgroup PRIMARY KEY (id)
 );
 
 CREATE TABLE contactgroup_member (
     contactgroup_id bigint NOT NULL REFERENCES contactgroup(id),
     contact_id bigint NOT NULL REFERENCES contact(id),
 
-    PRIMARY KEY (contactgroup_id, contact_id)
+    CONSTRAINT pk_contactgroup_member PRIMARY KEY (contactgroup_id, contact_id)
 );
 
 CREATE TABLE schedule (
-    id bigserial PRIMARY KEY,
-    name text NOT NULL
+    id bigserial,
+    name text NOT NULL,
+
+    CONSTRAINT pk_schedule PRIMARY KEY (id)
 );
 
 CREATE TABLE timeperiod (
-    id bigserial PRIMARY KEY,
-    owned_by_schedule_id bigint REFERENCES schedule(id) -- nullable for future standalone timeperiods
+    id bigserial,
+    owned_by_schedule_id bigint REFERENCES schedule(id), -- nullable for future standalone timeperiods
+
+    CONSTRAINT pk_timeperiod PRIMARY KEY (id)
 );
 
 CREATE TABLE timeperiod_entry (
-    id bigserial PRIMARY KEY,
+    id bigserial,
     timeperiod_id bigint NOT NULL REFERENCES timeperiod(id),
     start_time bigint NOT NULL,
     end_time bigint NOT NULL,
     timezone text NOT NULL, -- e.g. 'Europe/Berlin', relevant for evaluating rrule (DST changes differ between zones)
     rrule text, -- recurrence rule (RFC5545)
-    description text
+    description text,
+
+    CONSTRAINT pk_timeperiod_entry PRIMARY KEY (id)
 );
 
 CREATE TABLE schedule_member (
@@ -62,29 +73,35 @@ CREATE TABLE schedule_member (
 );
 
 CREATE TABLE channel (
-    id bigserial PRIMARY KEY,
+    id bigserial,
     name text NOT NULL,
     type text NOT NULL, -- 'email', 'sms', ...
-    config text -- JSON with channel-specific attributes
+    config text, -- JSON with channel-specific attributes
     -- for now type determines the implementation, in the future, this will need a reference to a concrete
     -- implementation to allow multiple implementations of a sms channel for example, probably even user-provided ones
+
+    CONSTRAINT pk_channel PRIMARY KEY (id)
 );
 
 CREATE TABLE source (
-    id bigserial PRIMARY KEY,
+    id bigserial,
     type text NOT NULL,
-    name text NOT NULL
+    name text NOT NULL,
     -- will likely need a distinguishing value for multiple sources of the same type in the future, like for example
     -- the Icinga DB environment ID for Icinga 2 sources
+
+    CONSTRAINT pk_source PRIMARY KEY (id)
 );
 
 CREATE TABLE object (
-    id bytea NOT NULL PRIMARY KEY, -- SHA256 of identifying tags
+    id bytea NOT NULL, -- SHA256 of identifying tags
     -- this will probably become more flexible in the future
     host text NOT NULL,
     service text,
 
-    CHECK (length(id) = 256/8)
+    CHECK (length(id) = 256/8),
+
+    CONSTRAINT pk_object PRIMARY KEY (id)
 );
 
 CREATE TABLE source_object (
@@ -93,7 +110,7 @@ CREATE TABLE source_object (
     name text NOT NULL,
     url text,
 
-    PRIMARY KEY (object_id, source_id)
+    CONSTRAINT pk_source_object PRIMARY KEY (object_id, source_id)
 );
 
 
@@ -104,14 +121,14 @@ CREATE TABLE object_extra_tag (
     tag text NOT NULL,
     value text,
 
-    PRIMARY KEY (object_id, source_id, tag),
+    CONSTRAINT pk_object_extra_tag PRIMARY KEY (object_id, source_id, tag),
     FOREIGN KEY (object_id, source_id) REFERENCES source_object(object_id, source_id)
 );
 
 CREATE TYPE severity AS ENUM ('ok', 'debug', 'info', 'notice', 'warning', 'err', 'crit', 'alert', 'emerg');
 
 CREATE TABLE event (
-    id bigserial PRIMARY KEY,
+    id bigserial,
     time bigint NOT NULL,
     source_id bigint NOT NULL REFERENCES source(id),
     object_id bytea NOT NULL REFERENCES object(id),
@@ -120,52 +137,61 @@ CREATE TABLE event (
     message text,
     username text,
 
+    CONSTRAINT pk_event PRIMARY KEY (id),
     FOREIGN KEY (object_id, source_id) REFERENCES source_object(object_id, source_id)
 );
 
 CREATE TABLE rule (
-    id bigserial PRIMARY KEY,
+    id bigserial,
     name text NOT NULL,
     timeperiod_id bigint REFERENCES timeperiod(id),
-    object_filter text
+    object_filter text,
+
+    CONSTRAINT pk_rule PRIMARY KEY (id)
 );
 
 CREATE TABLE rule_escalation (
-    id bigserial PRIMARY KEY,
+    id bigserial,
     rule_id bigint NOT NULL REFERENCES rule(id),
     position integer NOT NULL,
     condition text,
     name text, -- if not set, recipients are used as a fallback for display purposes
     fallback_for bigint REFERENCES rule_escalation(id),
 
+    CONSTRAINT pk_rule_escalation PRIMARY KEY (id),
+
     UNIQUE (rule_id, position),
     CHECK (NOT (condition IS NOT NULL AND fallback_for IS NOT NULL))
 );
 
 CREATE TABLE rule_escalation_recipient (
-    id bigserial PRIMARY KEY,
+    id bigserial,
     rule_escalation_id bigint NOT NULL REFERENCES rule_escalation(id),
     contact_id bigint REFERENCES contact(id),
     contactgroup_id bigint REFERENCES contactgroup(id),
     schedule_id bigint REFERENCES schedule(id),
     channel_type text NOT NULL,
 
+    CONSTRAINT pk_rule_escalation_recipient PRIMARY KEY (id),
+
     CHECK (num_nonnulls(contact_id, contactgroup_id, schedule_id) = 1)
 );
 
 CREATE TABLE incident (
-    id bigserial PRIMARY KEY,
+    id bigserial,
     object_id bytea NOT NULL REFERENCES object(id),
     started_at bigint NOT NULL,
     recovered_at bigint,
-    severity severity NOT NULL
+    severity severity NOT NULL,
+
+    CONSTRAINT pk_incident PRIMARY KEY (id)
 );
 
 CREATE TABLE incident_event (
     incident_id bigint NOT NULL REFERENCES incident(id),
     event_id bigint NOT NULL REFERENCES event(id),
 
-    PRIMARY KEY (incident_id, event_id)
+    CONSTRAINT pk_incident_event PRIMARY KEY (incident_id, event_id)
 );
 
 CREATE TYPE incident_contact_role AS ENUM ('recipient', 'subscriber', 'manager');
@@ -175,14 +201,14 @@ CREATE TABLE incident_contact (
     contact_id bigint NOT NULL REFERENCES contact(id),
     role incident_contact_role NOT NULL,
 
-    PRIMARY KEY (incident_id, contact_id)
+    CONSTRAINT pk_incident_contact PRIMARY KEY (incident_id, contact_id)
 );
 
 CREATE TABLE incident_rule (
     incident_id bigint NOT NULL REFERENCES incident(id),
     rule_id bigint NOT NULL REFERENCES rule(id),
 
-    PRIMARY KEY (incident_id, rule_id)
+    CONSTRAINT pk_incident_rule PRIMARY KEY (incident_id, rule_id)
 );
 
 CREATE TABLE incident_rule_escalation_state (
@@ -190,16 +216,17 @@ CREATE TABLE incident_rule_escalation_state (
     rule_escalation_id bigint NOT NULL REFERENCES rule_escalation(id),
     triggered_at bigint NOT NULL,
 
-    PRIMARY KEY (incident_id, rule_escalation_id)
+    CONSTRAINT pk_incident_rule_escalation_state PRIMARY KEY (incident_id, rule_escalation_id)
 );
 
 CREATE TABLE incident_history (
-    id bigserial PRIMARY KEY,
+    id bigserial,
     incident_id bigint NOT NULL REFERENCES incident(id),
     rule_escalation_id bigint REFERENCES rule_escalation(id),
     time bigint NOT NULL,
     -- unstructured history log for very early versions, will become more structured in the future
     message text NOT NULL,
 
+    CONSTRAINT pk_incident_history PRIMARY KEY (id),
     FOREIGN KEY (incident_id, rule_escalation_id) REFERENCES incident_rule_escalation_state(incident_id, rule_escalation_id)
 );
