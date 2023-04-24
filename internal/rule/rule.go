eeb3db17a0659584ad8c1e754291a97ea571a660
Yonas Habteab <yonas.habteab@icinga.com>
2023-04-24 15:38:21 +0200

    Replace `sql.NullInt64` by `types.Int`

--- internal/rule/rule.go
+++ internal/rule/rule.go
@@ -1,7 +1,6 @@
 package rule
 
 import (
-	"database/sql"
 	"github.com/icinga/icingadb/pkg/types"
 	"github.com/icinga/noma/internal/object"
 	"github.com/icinga/noma/internal/timeperiod"
@@ -12,7 +11,7 @@ type Rule struct {
 	IsActive         types.Bool `db:"is_active"`
 	Name             string     `db:"name"`
 	TimePeriod       *timeperiod.TimePeriod
-	TimePeriodID     sql.NullInt64  `db:"timeperiod_id"`
+	TimePeriodID     types.Int      `db:"timeperiod_id"`
 	ObjectFilter     *object.Filter `db:"-"`
 	ObjectFilterExpr types.String   `db:"object_filter"`
 	Escalations      []*Escalation
