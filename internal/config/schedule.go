1dc13320f0aec9fe3bfe27e3519112ed446eb4f5
Julian Brost <julian.brost@icinga.com>
2023-04-27 15:14:02 +0200

    Implement incremental config updates for schedules

--- internal/config/schedule.go
+++ internal/config/schedule.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"context"
-	"database/sql"
 	"github.com/icinga/icingadb/pkg/icingadb"
 	"github.com/icinga/icingadb/pkg/logging"
 	"github.com/icinga/noma/internal/recipient"
@@ -31,51 +30,101 @@ func (r *RuntimeConfig) fetchSchedules(ctx context.Context, db *icingadb.DB, tx 
 			zap.String("name", g.Name))
 	}
 
-	type ScheduleMember struct {
-		ScheduleID   int64         `db:"schedule_id"`
-		TimePeriodID int64         `db:"timeperiod_id"`
-		ContactID    sql.NullInt64 `db:"contact_id"`
-		GroupID      sql.NullInt64 `db:"contactgroup_id"`
-	}
-
-	var memberPtr *ScheduleMember
+	var memberPtr *recipient.ScheduleMemberRow
 	stmt = db.BuildSelectStmt(memberPtr, memberPtr)
 	log.Println(stmt)
 
-	var members []*ScheduleMember
+	var members []*recipient.ScheduleMemberRow
 	if err := tx.SelectContext(ctx, &members, stmt); err != nil {
 		log.Println(err)
 		return err
 	}
 
 	for _, member := range members {
-		memberLogger := logger.With(
-			zap.Int64("schedule_id", member.ScheduleID),
-			zap.Int64("timeperiod_id", member.TimePeriodID),
-			zap.Int64("contact_id", member.ContactID.Int64),
-			zap.Int64("contactgroup_id", member.GroupID.Int64),
-		)
+		memberLogger := makeScheduleMemberLogger(logger.SugaredLogger, member)
 
 		if s := schedulesById[member.ScheduleID]; s == nil {
 			memberLogger.Warnw("ignoring schedule member for unknown schedule_id")
-		} else if p := r.pending.TimePeriods[member.TimePeriodID]; p == nil {
-			memberLogger.Warnw("ignoring schedule member for unknown timeperiod_id")
-		} else if c := r.pending.Contacts[member.ContactID.Int64]; member.ContactID.Valid && p == nil {
-			memberLogger.Warnw("ignoring schedule member for unknown contact_id")
-		} else if g := r.pending.Groups[member.GroupID.Int64]; member.GroupID.Valid && p == nil {
-			memberLogger.Warnw("ignoring schedule member for unknown contactgroup_id")
 		} else {
-			s.Members = append(s.Members, &recipient.Member{
-				TimePeriod:   p,
-				Contact:      c,
-				ContactGroup: g,
-			})
+			s.MemberRows = append(s.MemberRows, member)
 
 			memberLogger.Debugw("member")
+		}
+	}
+
+	if r.Schedules != nil {
+		// mark no longer existing schedules for deletion
+		for id := range r.Schedules {
+			if _, ok := schedulesById[id]; !ok {
+				schedulesById[id] = nil
+			}
 		}
 	}
 
 	r.pending.Schedules = schedulesById
 
 	return nil
+}
+
+func (r *RuntimeConfig) applyPendingSchedules(logger *logging.Logger) {
+	if r.Schedules == nil {
+		r.Schedules = make(map[int64]*recipient.Schedule)
+	}
+
+	for id, pendingSchedule := range r.pending.Schedules {
+		if pendingSchedule == nil {
+			delete(r.Schedules, id)
+		} else {
+			for _, memberRow := range pendingSchedule.MemberRows {
+				memberLogger := makeScheduleMemberLogger(logger.SugaredLogger, memberRow)
+
+				period := r.TimePeriods[memberRow.TimePeriodID]
+				if period == nil {
+					memberLogger.Warnw("ignoring schedule member for unknown timeperiod_id")
+					continue
+				}
+
+				var contact *recipient.Contact
+				if memberRow.ContactID.Valid {
+					contact = r.Contacts[memberRow.ContactID.Int64]
+					if contact == nil {
+						memberLogger.Warnw("ignoring schedule member for unknown contact_id")
+						continue
+					}
+				}
+
+				var group *recipient.Group
+				if memberRow.GroupID.Valid {
+					group = r.Groups[memberRow.GroupID.Int64]
+					if group == nil {
+						memberLogger.Warnw("ignoring schedule member for unknown contactgroup_id")
+						continue
+					}
+				}
+
+				pendingSchedule.Members = append(pendingSchedule.Members, &recipient.Member{
+					TimePeriod:   period,
+					Contact:      contact,
+					ContactGroup: group,
+				})
+			}
+
+			if currentSchedule := r.Schedules[id]; currentSchedule != nil {
+				*currentSchedule = *pendingSchedule
+			} else {
+				r.Schedules[id] = pendingSchedule
+			}
+		}
+	}
+
+	r.pending.Schedules = nil
+}
+
+func makeScheduleMemberLogger(logger *zap.SugaredLogger, member *recipient.ScheduleMemberRow) *zap.SugaredLogger {
+	return logger.With(
+		zap.Int64("schedule_id", member.ScheduleID),
+		zap.Int64("timeperiod_id", member.TimePeriodID),
+		zap.Int64("contact_id", member.ContactID.Int64),
+		zap.Int64("contactgroup_id", member.GroupID.Int64),
+	)
 }
