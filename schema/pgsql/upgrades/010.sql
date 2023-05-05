8dca18e51d0bb350a22f80ef7fff51bafaad40b6
Julian Brost <julian.brost@icinga.com>
2023-05-05 11:11:02 +0200

    Build incident source severities from `event` & `incident_event` tables

--- schema/pgsql/upgrades/010.sql
+++ schema/pgsql/upgrades/010.sql
@@ -6,4 +6,8 @@ CREATE TABLE incident_source (
     CONSTRAINT pk_incident_source PRIMARY KEY (incident_id, source_id)
 );
 
-INSERT INTO incident_source (source_id, incident_id, severity) SELECT 1, id, severity FROM incident;
+INSERT INTO incident_source (incident_id, source_id, severity)
+SELECT DISTINCT ON (incident_event.incident_id, event.source_id) incident_event.incident_id, event.source_id, event.severity
+FROM incident_event JOIN event ON incident_event.event_id = event.id
+WHERE event.type = 'state'
+ORDER BY incident_event.incident_id, event.source_id, event.time DESC;
