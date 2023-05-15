1ecf0da5eae9f340285cd71d8de12d7b6d0efec5
raviks789 <33730024+raviks789@users.noreply.github.com>
2023-05-15 12:37:20 +0200

    Add `anynonarrayliketext` function in PostgreSQL to fix numeric value suggestion error

--- schema/pgsql/schema.sql
+++ schema/pgsql/schema.sql
@@ -2,6 +2,20 @@ CREATE TYPE boolenum AS ENUM ( 'n', 'y' );
 CREATE TYPE incident_history_event_type AS ENUM ( 'source_severity_changed', 'incident_severity_changed', 'recipient_role_changed', 'escalation_triggered', 'rule_matched', 'opened', 'closed', 'notified' );
 CREATE TYPE frequency_type AS ENUM ( 'MINUTELY', 'HOURLY', 'DAILY', 'WEEKLY', 'MONTHLY', 'QUARTERLY', 'YEARLY' );
 
+-- IPL ORM renders SQL queries with LIKE operators for all suggestions in the search bar,
+-- which fails for numeric and enum types on PostgreSQL. Just like in Icinga DB Web.
+CREATE OR REPLACE FUNCTION anynonarrayliketext(anynonarray, text)
+    RETURNS bool
+    LANGUAGE plpgsql
+    IMMUTABLE
+    PARALLEL SAFE
+    AS $$
+        BEGIN
+            RETURN $1::TEXT LIKE $2;
+        END;
+    $$;
+CREATE OPERATOR ~~ (LEFTARG=anynonarray, RIGHTARG=text, PROCEDURE=anynonarrayliketext);
+
 CREATE TABLE contact (
     id bigserial,
     full_name text NOT NULL,
