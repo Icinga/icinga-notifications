76ed6ee339cb34f47a7f8c8251ac9f69fbd4d6b3
Alvar Penning <alvar.penning@icinga.com>
2023-10-16 17:16:54 +0200

    eventstream: Initial Client
    So far, the Client has not much more logic as the initial testing main
    method. However, not it uses the previously defined types and is the
    initial building block for some looping type with an Event callback.

--- cmd/icinga2-notification-source/main.go
+++ cmd/icinga2-notification-source/main.go
@@ -1,66 +1,22 @@
 package main
 
 import (
+	"context"
 	"crypto/tls"
-	"encoding/json"
-	"io"
-	"log"
+	"fmt"
+	"github.com/icinga/icinga-notifications/internal/event"
+	"github.com/icinga/icinga-notifications/internal/eventstream"
 	"net/http"
-	"os"
-	"strings"
 )
 
 func main() {
-	req, err := http.NewRequest(http.MethodPost, "https://localhost:5665/v1/events", strings.NewReader(`{"queue":"icinga-notifications","types":["StateChange","AcknowledgementSet","AcknowledgementCleared"]}`))
-	if err != nil {
-		panic(err)
+	client := eventstream.Client{
+		ApiHost:          "https://localhost:5665",
+		ApiBasicAuthUser: "root",
+		ApiBasicAuthPass: "icinga",
+		ApiHttpTransport: http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}},
+		Ctx:              context.Background(),
+		CallbackFn:       func(event event.Event) { fmt.Println(event.FullString()) },
 	}
-
-	req.SetBasicAuth("root", "icinga")
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Accept", "application/json")
-
-	client := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: true,
-			},
-		},
-	}
-
-	res, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-
-	jsonR, jsonW := io.Pipe()
-	go func() {
-		_, err = io.Copy(io.MultiWriter(os.Stdout, jsonW), res.Body)
-		if err != nil {
-			panic(err)
-		}
-	}()
-
-	dec := json.NewDecoder(jsonR)
-	for {
-		var event Icinga2Event
-		err := dec.Decode(&event)
-		if err != nil {
-			panic(err)
-		}
-		log.Printf("%#v", &event)
-	}
-}
-
-type Icinga2Event struct {
-	Acknowledgement bool `json:"acknowledgement"`
-	CheckResult     struct {
-		Output string `json:"output"`
-	} `json:"check_result"`
-	Host      string  `json:"host"`
-	Service   string  `json:"service"`
-	State     int     `json:"state"`
-	StateType int     `json:"state_type"`
-	Timestamp float64 `json:"timestamp"`
-	Type      string  `json:"type"`
+	panic(client.ListenEventStream())
 }
