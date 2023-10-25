45e3021e72e7f51e5f5186652f273fb179120278
Yonas Habteab <yonas.habteab@icinga.com>
2023-10-25 11:29:02 +0200

    EscalationFilter: add ExtractRetryAfter()

--- internal/rule/condition.go
+++ internal/rule/condition.go
@@ -3,12 +3,39 @@ package rule
 import (
 	"fmt"
 	"github.com/icinga/icinga-notifications/internal/event"
+	"github.com/icinga/icinga-notifications/internal/filter"
+	"math"
 	"time"
 )
+
+// RetryNever indicates that an escalation condition should never be retried once it has been evaluated.
+const RetryNever = time.Duration(math.MaxInt64)
 
 type EscalationFilter struct {
 	IncidentAge      time.Duration
 	IncidentSeverity event.Severity
+}
+
+// ExtractRetryAfter extracts a time.Duration from the given filter conditions.
+// The retry after duration is extracted from the specified filter conditions that evaluate the "incident_age"
+// filter column. This function returns rule.RetryNever when all the incident age filter values are < the actual
+// incident age or none of the filter conditions evaluates the "incident_age" column.
+func (e *EscalationFilter) ExtractRetryAfter(conditions []filter.Condition, incidentAge time.Duration) time.Duration {
+	retryAfter := RetryNever
+	for _, condition := range conditions {
+		if condition.Column == "incident_age" {
+			age, err := time.ParseDuration(condition.Value)
+			if err != nil {
+				continue
+			}
+
+			if incidentAge < age {
+				retryAfter = min(retryAfter, age)
+			}
+		}
+	}
+
+	return retryAfter
 }
 
 func (e *EscalationFilter) EvalEqual(key string, value string) (bool, error) {
