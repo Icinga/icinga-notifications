9181e7e358a2495510fb3fa262b82afc9120dd65
Yonas Habteab <yonas.habteab@icinga.com>
2024-05-17 14:37:56 +0200

    Introduce and use `notification` package

--- internal/incident/db_types.go
+++ internal/incident/db_types.go
@@ -65,22 +65,20 @@ func (r *RuleRow) TableName() string {
 
 // HistoryRow represents a single incident history database entry.
 type HistoryRow struct {
-	ID                int64     `db:"id"`
-	IncidentID        int64     `db:"incident_id"`
-	RuleEntryID       types.Int `db:"rule_entry_id"`
-	EventID           types.Int `db:"event_id"`
-	recipient.Key     `db:",inline"`
-	RuleID            types.Int         `db:"rule_id"`
-	Time              types.UnixMilli   `db:"time"`
-	Type              HistoryEventType  `db:"type"`
-	ChannelID         types.Int         `db:"channel_id"`
-	NewSeverity       event.Severity    `db:"new_severity"`
-	OldSeverity       event.Severity    `db:"old_severity"`
-	NewRecipientRole  ContactRole       `db:"new_recipient_role"`
-	OldRecipientRole  ContactRole       `db:"old_recipient_role"`
-	Message           types.String      `db:"message"`
-	NotificationState NotificationState `db:"notification_state"`
-	SentAt            types.UnixMilli   `db:"sent_at"`
+	ID                    int64     `db:"id"`
+	IncidentID            int64     `db:"incident_id"`
+	RuleEntryID           types.Int `db:"rule_entry_id"`
+	EventID               types.Int `db:"event_id"`
+	recipient.Key         `db:",inline"`
+	RuleID                types.Int        `db:"rule_id"`
+	NotificationHistoryID types.Int        `db:"notification_history_id"`
+	Time                  types.UnixMilli  `db:"time"`
+	Type                  HistoryEventType `db:"type"`
+	NewSeverity           event.Severity   `db:"new_severity"`
+	OldSeverity           event.Severity   `db:"old_severity"`
+	NewRecipientRole      ContactRole      `db:"new_recipient_role"`
+	OldRecipientRole      ContactRole      `db:"old_recipient_role"`
+	Message               types.String     `db:"message"` // Is only used to store Incident (un)mute reason.
 }
 
 // TableName implements the contracts.TableNamer interface.
@@ -99,23 +97,4 @@ func (h *HistoryRow) Sync(ctx context.Context, db *database.DB, tx *sqlx.Tx) err
 	h.ID = historyId
 
 	return nil
-}
-
-// NotificationEntry is used to cache a set of incident history fields of type Notified.
-//
-// The event processing workflow is performed in a separate transaction before trying to send the actual
-// notifications. Thus, all resulting notification entries are marked as pending, and it creates a reference
-// to them of this type. The cached entries are then used to actually notify the contacts and mark the pending
-// notification entries as either NotificationStateSent or NotificationStateFailed.
-type NotificationEntry struct {
-	HistoryRowID int64             `db:"id"`
-	ContactID    int64             `db:"-"`
-	ChannelID    int64             `db:"-"`
-	State        NotificationState `db:"notification_state"`
-	SentAt       types.UnixMilli   `db:"sent_at"`
-}
-
-// TableName implements the contracts.TableNamer interface.
-func (h *NotificationEntry) TableName() string {
-	return "incident_history"
 }
