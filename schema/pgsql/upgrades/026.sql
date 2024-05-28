6fb24b88abe5add0e25dbc3469d25d8553227c1b
Julian Brost <julian.brost@icinga.com>
2024-05-28 13:27:45 +0200

    Simplify schema upgrade for schedule rotations
    Reduce the schema file to the minimal changes that are needed to go from the
    old to the new version. This makes it easier to follow what has been changed
    and also allows keeping other timeperiods (if they were manually created as it
    was not yet possible to create those outside of schedules using the web
    interface so far). Comments were removed from here as they still exist in the
    full schema file and the purpose of the upgrade file is not to document the
    schema.

--- schema/pgsql/upgrades/026.sql
+++ schema/pgsql/upgrades/026.sql
@@ -1,48 +1,18 @@
-DO $$ BEGIN
-    CREATE TYPE rotation_type AS ENUM ( '24-7', 'partial', 'multi' );
-EXCEPTION
-    WHEN duplicate_object THEN null;
-END $$;
+-- IMPORTANT: This schema upgrade removes all schedule-related configuration as it was changed in an incompatible way!
 
-CREATE TABLE IF NOT EXISTS rotation (
+CREATE TYPE rotation_type AS ENUM ( '24-7', 'partial', 'multi' );
+
+CREATE TABLE rotation (
     id bigserial,
     schedule_id bigint NOT NULL REFERENCES schedule(id),
-    -- the lower the more important, starting at 0, avoids the need to re-index upon addition
     priority integer NOT NULL,
     name text NOT NULL,
     mode rotation_type NOT NULL,
-    -- JSON with rotation-specific attributes
-    -- Needed exclusively by Web to simplify editing and visualisation
     options text NOT NULL,
-
-    -- A date in the format 'YYYY-MM-DD' when the first handoff should happen.
-    -- It is a string as handoffs are restricted to happen only once per day
     first_handoff date NOT NULL,
-
-    -- Set to the actual time of the first handoff.
-    -- If this is in the past during creation of the rotation, it is set to the creation time.
-    -- Used by Web to avoid showing shifts that never happened
     actual_handoff bigint NOT NULL,
-
-    -- each schedule can only have one rotation with a given priority starting at a given date
     UNIQUE (schedule_id, priority, first_handoff),
-
     CONSTRAINT pk_rotation PRIMARY KEY (id)
-);
-
-ALTER TABLE rule DROP COLUMN timeperiod_id;
-
-DROP TABLE IF EXISTS schedule_member;
-DROP TABLE IF EXISTS rotation_member;
-
-DROP TABLE IF EXISTS timeperiod_entry;
-
-DROP TABLE timeperiod;
-CREATE TABLE timeperiod (
-    id bigserial,
-    owned_by_rotation_id bigint REFERENCES rotation(id), -- nullable for future standalone timeperiods
-
-    CONSTRAINT pk_timeperiod PRIMARY KEY (id)
 );
 
 CREATE TABLE rotation_member (
@@ -51,38 +21,25 @@ CREATE TABLE rotation_member (
     contact_id bigint REFERENCES contact(id),
     contactgroup_id bigint REFERENCES contactgroup(id),
     position integer NOT NULL,
-
-    UNIQUE (rotation_id, position), -- each position in a rotation can only be used once
-
-    -- Two UNIQUE constraints prevent duplicate memberships of the same contact or contactgroup in a single rotation.
-    -- Multiple NULLs are not considered to be duplicates, so rows with a contact_id but no contactgroup_id are
-    -- basically ignored in the UNIQUE constraint over contactgroup_id and vice versa. The CHECK constraint below
-    -- ensures that each row has only non-NULL values in one of these constraints.
+    UNIQUE (rotation_id, position),
     UNIQUE (rotation_id, contact_id),
     UNIQUE (rotation_id, contactgroup_id),
     CHECK (num_nonnulls(contact_id, contactgroup_id) = 1),
-
     CONSTRAINT pk_rotation_member PRIMARY KEY (id)
 );
 
-CREATE TABLE timeperiod_entry (
-    id bigserial,
-    timeperiod_id bigint NOT NULL REFERENCES timeperiod(id),
-    rotation_member_id bigint REFERENCES rotation_member(id), -- nullable for future standalone timeperiods
-    start_time bigint NOT NULL,
-    end_time bigint NOT NULL,
-    -- Is needed by icinga-notifications-web to prefilter entries, which matches until this time and should be ignored by the daemon.
-    until_time bigint,
-    timezone text NOT NULL, -- e.g. 'Europe/Berlin', relevant for evaluating rrule (DST changes differ between zones)
-    rrule text, -- recurrence rule (RFC5545)
+DROP TABLE schedule_member;
 
-    CONSTRAINT pk_timeperiod_entry PRIMARY KEY (id)
-);
+DELETE FROM timeperiod_entry WHERE timeperiod_id IN (SELECT id FROM timeperiod WHERE owned_by_schedule_id IS NOT NULL);
+DELETE FROM timeperiod WHERE owned_by_schedule_id IS NOT NULL;
 
-ALTER TABLE rule ADD COLUMN timeperiod_id bigint REFERENCES timeperiod(id);
+ALTER TABLE timeperiod
+    DROP COLUMN owned_by_schedule_id,
+    ADD COLUMN owned_by_rotation_id bigint REFERENCES rotation(id);
 
-DO $$ BEGIN
-    DROP TYPE frequency_type;
-EXCEPTION
-    WHEN undefined_object THEN null;
-END $$;
+ALTER TABLE timeperiod_entry
+    DROP COLUMN frequency,
+    DROP COLUMN description,
+    ADD COLUMN rotation_member_id bigint REFERENCES rotation_member(id);
+
+DROP TYPE frequency_type;
