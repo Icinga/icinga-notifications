fecdc3449e71fce6ad4a5037d0a19f38c033d000
Yonas Habteab <yonas.habteab@icinga.com>
2024-05-31 11:32:51 +0200

    Drop unused `Remove*` utils functions

--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -5,28 +5,6 @@ import (
 	"testing"
 )
 
-func TestRemoveNils(t *testing.T) {
-	var a, b, c, d int
-
-	tests := []struct {
-		name string
-		in   []*int
-		want []*int
-	}{
-		{"Empty", []*int{}, []*int{}},
-		{"SingleKeep", []*int{&a}, []*int{&a}},
-		{"SingleRemove", []*int{nil}, []*int{}},
-		{"KeepOrder", []*int{&a, &b, &c, &d}, []*int{&a, &b, &c, &d}},
-		{"Duplicates", []*int{&a, &b, &b}, []*int{&a, &b, &b}},
-		{"Mixed", []*int{&a, nil, &b, nil, nil, &b, nil, &d}, []*int{&a, &b, &b, &d}},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.want, RemoveNils(tt.in))
-		})
-	}
-}
-
 func TestIterateOrderedMap(t *testing.T) {
 	tests := []struct {
 		name    string
