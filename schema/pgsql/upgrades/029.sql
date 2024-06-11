6b1a76bbec1d8aaa430188cbf307f44363adc53f
Noé Costa <noe.costa@icinga.com>
2024-06-11 10:37:02 +0200

    refactor: drop default statement for `authenticated_at`
    The PHP daemon now supplies the unix timestamp: https://github.com/Icinga/icinga-notifications-web/pull/146/commits/d1db5d4b174223ee52634d849b917540bdb6953d

--- schema/pgsql/upgrades/029.sql
+++ schema/pgsql/upgrades/029.sql
@@ -9,5 +9,8 @@ ALTER TABLE browser_session
 ALTER TABLE browser_session
     ADD CONSTRAINT pk_browser_session PRIMARY KEY (php_session_id);
 
+ALTER TABLE IF EXISTS browser_session
+    ALTER COLUMN authenticated_at DROP DEFAULT;
+
 CREATE INDEX idx_browser_session_authenticated_at ON browser_session (authenticated_at DESC);
 CREATE INDEX idx_browser_session_username_agent ON browser_session (username, user_agent);
