3989f7112117bd301012d018f85102bac1841029
Alvar Penning <alvar.penning@icinga.com>
2024-07-15 16:40:25 +0200

    Channel default value by changing default logic
    In a nutshell, the newly introduced plugin.PopulateDefaults function
    populates all fields of a Plugin-implementing struct with those fields
    from Info.ConfigAttributes where ConfigOption.Default is set. Thus, a
    single function call before parsing the user-submitted configuration
    within the Plugin.SetConfig method sets default values.
    
    As a result of the discussion between the Go and the Web team,
    summarized in #205, Web does not store key-value pairs to be omitted.
    
    Prior, an already JSON-encoded version of the ConfigOption slice was
    present in plugin.Info. Thus, this data wasn't easily available anymore.
    As the new code now needs to access this field, it was changed and a
    custom sql driver.Valuer was implemented for a slice type.
    
    While working on this, all ConfigOptions were put in the same order as
    the struct fields.
    
    Requires <https://github.com/Icinga/icinga-notifications-web/pull/230>.
    
    Closes #205.

---
+++ cmd/channels/email/main_test.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestEmail_SetConfig(t *testing.T) {
+	tests := []struct {
+		name    string
+		jsonMsg string
+		want    *Email
+		wantErr bool
+	}{
+		{
+			name:    "empty-string",
+			jsonMsg: ``,
+			wantErr: true,
+		},
+		{
+			name:    "empty-json-obj-use-defaults",
+			jsonMsg: `{}`,
+			want:    &Email{SenderName: "Icinga", SenderMail: "icinga@example.com"},
+		},
+		{
+			name:    "sender-mail-null-equals-defaults",
+			jsonMsg: `{"sender_mail": null}`,
+			want:    &Email{SenderName: "Icinga", SenderMail: "icinga@example.com"},
+		},
+		{
+			name:    "sender-mail-overwrite",
+			jsonMsg: `{"sender_mail": "foo@bar"}`,
+			want:    &Email{SenderName: "Icinga", SenderMail: "foo@bar"},
+		},
+		{
+			name:    "sender-mail-overwrite-empty",
+			jsonMsg: `{"sender_mail": ""}`,
+			want:    &Email{SenderName: "Icinga", SenderMail: ""},
+		},
+		{
+			name:    "full-example-config",
+			jsonMsg: `{"sender_name":"icinga","sender_mail":"icinga@example.com","host":"smtp.example.com","port":"25","encryption":"none"}`,
+			want: &Email{
+				Host:       "smtp.example.com",
+				Port:       "25",
+				SenderName: "icinga",
+				SenderMail: "icinga@example.com",
+				User:       "",
+				Password:   "",
+				Encryption: "none",
+			},
+		},
+		{
+			name:    "user-but-missing-pass",
+			jsonMsg: `{"user": "foo"}`,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			email := &Email{}
+			err := email.SetConfig(json.RawMessage(tt.jsonMsg))
+			assert.Equal(t, tt.wantErr, err != nil, "SetConfig() error = %v, wantErr = %t", err, tt.wantErr)
+			if err != nil {
+				return
+			}
+
+			assert.Equal(t, tt.want, email, "Email differs")
+		})
+	}
+}
