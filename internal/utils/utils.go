0167a0879e7449a4d501b67b45907ebc5f1efc65
Alvar Penning <alvar.penning@icinga.com>
2025-05-08 09:32:29 +0200

    internal/utils: Cleanup and IGL unification
    With https://github.com/Icinga/icinga-go-library/pull/64 and
    https://github.com/Icinga/icinga-go-library/pull/127 being merged, most
    of the internal/utils package is moved to the IGL and can be removed
    here. This is another step to unify our Go codebase.

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,85 +1,12 @@
 package utils
 
 import (
-	"cmp"
 	"context"
-	"database/sql"
 	"fmt"
 	"github.com/icinga/icinga-go-library/database"
-	"github.com/icinga/icinga-go-library/types"
 	"github.com/jmoiron/sqlx"
 	"github.com/pkg/errors"
-	"iter"
-	"slices"
-	"strings"
 )
-
-// BuildInsertStmtWithout builds an insert stmt without the provided column.
-func BuildInsertStmtWithout(db *database.DB, into interface{}, withoutColumn string) string {
-	columns := db.BuildColumns(into)
-	for i, column := range columns {
-		if column == withoutColumn {
-			// Event id is auto incremented, so just erase it from our insert columns
-			columns = append(columns[:i], columns[i+1:]...)
-			break
-		}
-	}
-
-	return fmt.Sprintf(
-		`INSERT INTO "%s" ("%s") VALUES (%s)`,
-		database.TableName(into), strings.Join(columns, `", "`),
-		fmt.Sprintf(":%s", strings.Join(columns, ", :")),
-	)
-}
-
-// RunInTx allows running a function in a database transaction without requiring manual transaction handling.
-//
-// A new transaction is started on db which is then passed to fn. After fn returns, the transaction is
-// committed unless an error was returned. If fn returns an error, that error is returned, otherwise an
-// error is returned if a database operation fails.
-func RunInTx(ctx context.Context, db *database.DB, fn func(tx *sqlx.Tx) error) error {
-	tx, err := db.BeginTxx(ctx, nil)
-	if err != nil {
-		return err
-	}
-	defer func() { _ = tx.Rollback() }()
-
-	err = fn(tx)
-	if err != nil {
-		return err
-	}
-
-	return tx.Commit()
-}
-
-// InsertAndFetchId executes the given query and fetches the last inserted ID.
-func InsertAndFetchId(ctx context.Context, tx *sqlx.Tx, stmt string, args any) (int64, error) {
-	var lastInsertId int64
-	if tx.DriverName() == database.PostgreSQL {
-		preparedStmt, err := tx.PrepareNamedContext(ctx, stmt+" RETURNING id")
-		if err != nil {
-			return 0, err
-		}
-		defer func() { _ = preparedStmt.Close() }()
-
-		err = preparedStmt.Get(&lastInsertId, args)
-		if err != nil {
-			return 0, fmt.Errorf("failed to insert entry for type %T: %w", args, err)
-		}
-	} else {
-		result, err := tx.NamedExecContext(ctx, stmt, args)
-		if err != nil {
-			return 0, fmt.Errorf("failed to insert entry for type %T: %w", args, err)
-		}
-
-		lastInsertId, err = result.LastInsertId()
-		if err != nil {
-			return 0, fmt.Errorf("failed to fetch last insert id for type %T: %w", args, err)
-		}
-	}
-
-	return lastInsertId, nil
-}
 
 // ExecAndApply applies the provided restoreFunc callback for each successfully retrieved row of the specified type.
 // Returns error on any database failure or fails to acquire the table semaphore.
@@ -124,44 +51,4 @@ func ForEachRow[Row, Id any](ctx context.Context, db *database.DB, idColumn stri
 	}
 
 	return ExecAndApply(ctx, db, stmt, args, restoreFunc)
-}
-
-// ToDBString transforms the given string to types.String.
-func ToDBString(value string) types.String {
-	str := types.String{NullString: sql.NullString{String: value}}
-	if value != "" {
-		str.Valid = true
-	}
-
-	return str
-}
-
-// ToDBInt transforms the given value to types.Int.
-func ToDBInt(value int64) types.Int {
-	val := types.Int{NullInt64: sql.NullInt64{Int64: value}}
-	if value != 0 {
-		val.Valid = true
-	}
-
-	return val
-}
-
-// IterateOrderedMap implements iter.Seq2 to iterate over a map in the key's order.
-//
-// This function returns a func yielding key-value-pairs from a given map in the order of their keys, if their type
-// is cmp.Ordered.
-func IterateOrderedMap[K cmp.Ordered, V any](m map[K]V) iter.Seq2[K, V] {
-	keys := make([]K, 0, len(m))
-	for key := range m {
-		keys = append(keys, key)
-	}
-	slices.Sort(keys)
-
-	return func(yield func(K, V) bool) {
-		for _, key := range keys {
-			if !yield(key, m[key]) {
-				return
-			}
-		}
-	}
 }
