8cf406743599c082e9b01fb5fa27969c909f1de6
Johannes Rauh <johannes.rauh@icinga.com>
2025-10-28 08:24:57 +0100

    Add default value for `schedule.timezone` upgrades
    The value must not be null, so we have to set a default when
    upgrading the schema. For that we look for the first
    `timeperiod_entry` and use it's `timezone` column. If no entry
    exists, the fallback is **'UTC'**.

--- schema/mysql/upgrades/0.2.0-schedule-timezone.sql
+++ schema/mysql/upgrades/0.2.0-schedule-timezone.sql
@@ -1,1 +1,12 @@
-ALTER TABLE schedule ADD COLUMN timezone text NOT NULL AFTER name;
+ALTER TABLE schedule ADD COLUMN timezone text AFTER name;
+UPDATE schedule SET timezone = (
+    SELECT entry.timezone
+    FROM timeperiod_entry entry
+    INNER JOIN timeperiod ON timeperiod.id = entry.timeperiod_id
+    INNER JOIN rotation ON rotation.id = timeperiod.owned_by_rotation_id
+    WHERE rotation.schedule_id = schedule.id
+    ORDER BY entry.id
+    LIMIT 1
+);
+UPDATE schedule SET timezone = 'UTC' WHERE timezone IS NULL;
+ALTER TABLE schedule MODIFY COLUMN timezone text NOT NULL;
