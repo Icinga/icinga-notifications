cd09f904e25efd260ac56988df36d98f7c4d75a9
Julian Brost <julian.brost@icinga.com>
2025-11-13 10:28:08 +0100

    Merge schema upgrade files for v0.2.0
    This gives a single file that can be applied during the upgrade more easily,
    having it in individual files was mostly to avoid merge conflicts during
    development.
    
    The combined files were created with the following command:
    
      cat 0.2.0-external-uuid.sql <(echo) 0.2.0-source-rules.sql <(echo) 0.2.0-schedule-timezone.sql <(echo) 0.2.0-source-username.sql > 0.2.0.sql
    
    The order of files matches the order in which the PRs that added them were
    merged (though the order shouldn't really matter as there are no dependencies
    between the individual files):
    
     - https://github.com/Icinga/icinga-notifications/pull/216
     - https://github.com/Icinga/icinga-notifications/pull/324
     - https://github.com/Icinga/icinga-notifications/pull/344
     - https://github.com/Icinga/icinga-notifications/pull/338

--- schema/pgsql/upgrades/0.2.0-source-username.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE source ADD COLUMN listener_username varchar(255);
-UPDATE source SET listener_username = CONCAT('source-', source.id) WHERE deleted = 'n';
-ALTER TABLE source
-    ADD CONSTRAINT uk_source_listener_username UNIQUE (listener_username),
-    ADD CONSTRAINT ck_source_listener_username_or_deleted CHECK (deleted = 'y' OR listener_username IS NOT NULL);
