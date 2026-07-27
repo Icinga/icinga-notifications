d412b5624a6f122ec0dcb2e7fb8349d0d012c515
Jan Schuppik <jan.schuppik@icinga.com>
2026-07-27 10:23:37 +0200

    add notification skipped history table

--- schema/pgsql/upgrades/notification-history.sql
+++ schema/pgsql/upgrades/notification-history.sql
@@ -37,3 +37,23 @@ CREATE TABLE notification_history (
 
 CREATE INDEX idx_notification_history_time ON notification_history(triggered_at);
 CREATE INDEX idx_notification_history_incident_id ON notification_history(incident_id);
+
+CREATE TABLE notification_skipped_history (
+    id bigint NOT NULL AUTO_INCREMENT,
+    notification_id bigint NOT NULL,
+    rule_id bigint NOT NULL,
+    rule_escalation_id bigint NOT NULL,
+    incident_id bigint,
+    contactgroup_id bigint,
+    schedule_id bigint,
+
+    CONSTRAINT pk_notification_skipped_history PRIMARY KEY (id),
+    CONSTRAINT fk_notification_skipped_history_notification FOREIGN KEY (notification_id) REFERENCES notification_history(id),
+    CONSTRAINT fk_notification_skipped_history_rule FOREIGN KEY (rule_id) REFERENCES rule(id),
+    CONSTRAINT fk_notification_skipped_history_rule_escalation FOREIGN KEY (rule_escalation_id) REFERENCES rule_escalation(id),
+    CONSTRAINT fk_notification_skipped_history_incident FOREIGN KEY (incident_id) REFERENCES incident(id),
+    CONSTRAINT fk_notification_skipped_history_contactgroup FOREIGN KEY (contactgroup_id) REFERENCES contactgroup(id),
+    CONSTRAINT fk_notification_skipped_history_schedule FOREIGN KEY (schedule_id) REFERENCES schedule(id)
+);
+
+CREATE INDEX idx_notification_skipped_history_incident_id ON notification_skipped_history(notification_id);
